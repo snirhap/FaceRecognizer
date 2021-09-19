eb2547f74cf4f1b66de07e21ba27ae65e8b26a9d
Snir Ohayon <snir@varada.io>
2021-09-19 20:53:12 +0300

    Add requirements.txt, make best matching to work with threads

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import time
+from queue import Queue
+from threading import Thread
 from faker import Faker
 from numpy import dot, linalg, random
 from flask import Flask, request, jsonify
@@ -10,9 +13,8 @@ NUM_OF_FEATURES = 256
 PROXIMATE = 5
 DB_NAME = "DB"
 PERSONS_COLLECTION_NAME = "persons"
-MAX_PERSONS_IN_COLLECTION = 200
-
-# NUM_OF_FEATURES = 256
+NUM_OF_THREADS = 3
+MAX_PERSONS_IN_COLLECTION = 10000
 
 app = Flask(__name__)
 db_handler = MongoDBHandler(DB_NAME)
@@ -22,33 +24,109 @@ def vector_generator():
     return [round(feature, PROXIMATE) for feature in random.rand(NUM_OF_FEATURES).tolist()]
 
 
-# def normalize_vector(vector_to_normalize):
-#     return (vector_to_normalize / linalg.norm(vector_to_normalize, axis=-1, ord=2)).tolist()
-
-
 def get_top_matches(compare):
-    return sorted(compare, key=lambda k: k['result'], reverse=True)
+    return sorted(compare, key=lambda k: k['result'], reverse=True)[:DESIRED_TOP_MATCHES]
 
 
 @app.route('/', methods=['GET'])
-def face_recognition():
-    compare = []
+def get_closest_match():
     input_data = request.get_json()
     if input_data:
         message, status_code = get_person_input_validation(input_data)
         if status_code == status.HTTP_200_OK:
             features = request.get_json()["features"]
-            for person in db_handler.scan_collection(PERSONS_COLLECTION_NAME):
-                dot_prod = dot(features, person["features"])
-                # Cosine similarity
-                cos_sim = round(dot_prod / (linalg.norm(features) * linalg.norm(person["features"])), PROXIMATE)
-                compare.append({"person_name": person["person_name"], "result": cos_sim})
-            top_matches = get_top_matches(compare)
-            return jsonify(top_matches[:DESIRED_TOP_MATCHES])
+            all_persons_in_collection = db_handler.scan_collection(PERSONS_COLLECTION_NAME)
+            # Parallel best matches
+            start = time.time()
+            top_matches = get_similarity_multi_threaded(features, all_persons_in_collection)
+            end = time.time()
+            print(f"Runtime Multi Thread: {end - start}")
+            print(f"Top Matches Multi Thread: {top_matches}")
+
+            # # single thread
+            # start = time.time()
+            # top_matches = get_similarities_for_query_vector(features, all_persons_in_collection)
+            # end = time.time()
+            # print(f"Runtime Single Thread: {end - start}")
+            # print(f"Top Matches Single Thread: {top_matches}")
+            return jsonify(top_matches)
         else:
             return f'Invalid input, {message}', status.HTTP_400_BAD_REQUEST
     else:
         return jsonify(db_handler.scan_collection(PERSONS_COLLECTION_NAME))
+
+
+def get_similarity_multi_threaded(query_vector: list, collection_vectors: list):
+    workers = []
+    processed_results = []
+    queue_to_process = Queue()
+
+    # Fill queue with all persons
+    for element in collection_vectors:
+        queue_to_process.put(element)
+
+    for i in range(NUM_OF_THREADS):
+        worker = Thread(target=process_queue_elements, args=(queue_to_process, query_vector, processed_results))
+        workers.append(worker)
+        worker.start()
+
+    for worker in workers:
+        worker.join()
+
+    return get_top_matches(processed_results)
+
+
+def process_queue_elements(queue_to_process: Queue, query_vector: list, processed_results: list):
+    while not queue_to_process.empty():
+        queue_vector = queue_to_process.get()
+        processed_results.append(get_similarity_for_query_vector(query_vector, queue_vector))
+
+
+def get_similarity_for_query_vector(query_vector: list, queue_vector: dict):
+    return {"person_name": queue_vector["person_name"],
+            "result": get_features_similarity_between_two_vectors(query_vector, queue_vector["features"])}
+
+
+def get_features_similarity_between_two_vectors(first_vector: list, second_vector: list):
+    # Calculate cosine similarity between 2 features vector
+    dot_prod = dot(first_vector, second_vector)
+    return round(dot_prod / (linalg.norm(first_vector) * linalg.norm(second_vector)), PROXIMATE)
+
+
+# Single thread
+def get_similarities_for_query_vector(query_vector: list, collection_vectors: list):
+    compare = []
+    for person in collection_vectors:
+        compare.append({"person_name": person["person_name"],
+                        "result": get_features_similarity_between_two_vectors(query_vector, person["features"])})
+    return get_top_matches(compare)
+
+# def get_similarity_multi_threaded(query_vector: list, collection_vectors: list):
+#     threads = []
+#     threads_results = []
+#     number_of_threads = 5
+#     with ThreadPoolContext(number_of_threads) as tp_ctx:
+#         chunk_size = db_handler.count_documents_in_collection(PERSONS_COLLECTION_NAME) // number_of_threads
+#         for chunk in divide_list_into_chunks(collection_vectors, chunk_size):
+#             threads.append(tp_ctx.apply_async(get_similarities_for_query_vector, (query_vector, chunk,)))
+#
+#     for thread in threads:
+#         threads_results.extend(thread.get())
+#
+#     return get_top_matches(threads_results)
+    # threads_results = []
+    # with futures.ThreadPoolExecutor() as executor:
+    #     # fixme 10
+    #     chunk_size = db_handler.count_documents_in_collection(PERSONS_COLLECTION_NAME) // 10
+    #     for chunk in divide_list_into_chunks(collection_vectors, chunk_size):
+    #         future = executor.submit(get_similarities_for_query_vector, query_vector, chunk)
+    #         threads_results.extend(future.result())
+    # return get_top_matches(threads_results)
+
+
+def divide_list_into_chunks(vector: list, number_of_chunks: int):
+    for i in range(0, len(vector), number_of_chunks):
+        yield vector[i: i + number_of_chunks]
 
 
 @app.route('/add', methods=['POST'])
@@ -61,8 +139,8 @@ def add_one_person():
     message, status_code = add_person_input_validation(input_data)
 
     if status_code == status.HTTP_200_OK:
-        # Check duplicate records
-        # ...
+        # Check duplicate records?
+        # db_handler.get_collection(PERSONS_COLLECTION_NAME).aggregate()
 
         db_handler.insert_one_to_collection(PERSONS_COLLECTION_NAME,
                                             {"person_name": input_data.get('person_name'),
@@ -104,12 +182,11 @@ def add_person_input_validation(input_data):
 
 def fill_db():
     fake = Faker()
-    fake_persons = [{"person_name": fake.name(), "features": vector_generator()} for _ in range(100)]
+    fake_persons = [{"person_name": fake.name(), "features": vector_generator()} for _ in range(5000)]
     db_handler.get_collection(PERSONS_COLLECTION_NAME).insert_many(fake_persons)
 
 
 if __name__ == '__main__':
-    # fill_db()
     # db_handler.truncate_collection(PERSONS_COLLECTION_NAME)
     # fill_db()
     app.run(debug=True)
--- db.py
+++ db.py
@@ -1,25 +1,25 @@
-import pymongo
+from pymongo import database, MongoClient, collection
 from bson.json_util import dumps
 from flask import json
 
 
 class MongoDBHandler:
     def __init__(self, database_name: str):
-        self.mongo = pymongo.MongoClient('mongodb+srv://admin:KpGbKaygcFlKoDRJ@facerecognition.3mbw1.mongodb.net/DB?retryWrites=true&w=majority',
-                                         maxPoolSize=50, connect=False)
-        self.db = pymongo.database.Database(self.mongo, database_name)
+        self.mongo = MongoClient('mongodb+srv://admin:KpGbKaygcFlKoDRJ@facerecognition.3mbw1.mongodb.net/DB?retryWrites=true&w=majority',
+                                 maxPoolSize=50, connect=False)
+        self.db = database.Database(self.mongo, database_name)
 
-    def truncate_collection(self, collection):
-        self.get_collection(collection).delete_many({})
+    def truncate_collection(self, collection_name: str):
+        self.get_collection(collection_name).delete_many({})
 
-    def get_collection(self, collection):
-        return pymongo.collection.Collection(self.db, collection)
+    def get_collection(self, collection_name: str):
+        return collection.Collection(self.db, collection_name)
 
-    def insert_one_to_collection(self, collection, data):
-        self.get_collection(collection).insert_one(data)
+    def insert_one_to_collection(self, collection_name: str, data):
+        self.get_collection(collection_name).insert_one(data)
 
-    def scan_collection(self, collection):
-        return json.loads(dumps(pymongo.collection.Collection(self.db, collection).find()))
+    def scan_collection(self, collection_name: str):
+        return json.loads(dumps(collection.Collection(self.db, collection_name).find()))
 
-    def count_documents_in_collection(self, collection: str):
-        return self.get_collection(collection).count_documents({})
+    def count_documents_in_collection(self, collection_name: str):
+        return self.get_collection(collection_name).count_documents({})
